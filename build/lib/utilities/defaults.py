ef1512e188abfe4178d4d4ef5e24407fd1eaac40
Frosty Gecko <imanfoster@gmail.com>
2024-09-18 11:52:31 -0600

    setup update

--- build/lib/utilities/defaults.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-import library.bodies as body
-
-radius              = body.planets['earth']['radius']
-mu                  = body.planets['earth']['mu']
-GMAT_date_format    = "%d %b %Y %H:%M:%S.000"
-nominal_date_format = "%Y-%m-%d %H:%M:%S "
